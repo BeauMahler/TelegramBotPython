1d54cc46ac1436945750afdf59d9b8bcab0f0734
Paul2803k <p.scattolin95@gmail.com>
2021-11-24 21:10:22 +0100

    first try to append data to a jpg image

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+import io
+import PIL.Image as Image
 
 
-# Press the green button in the gutter to run the script.
+def read_image(path):
+    with open(path, "rb") as f:
+        return bytearray(f.read())
+
+
+def main():
+    cwd = os.getcwd()
+
+    input_list = [(1, 2), (3, 4), (12, 4)]
+
+    byte_list = []
+
+    # convert input_list
+    for el in input_list:
+        temp = str(el[0]).encode() + str(el[1]).encode()
+        byte_list.insert(len(byte_list), temp)
+
+    secret = b''.join(byte_list)
+
+    print(secret)
+
+    # bytes_image = read_image(cwd + '\\resources\\original.jpg')
+    # print(bytes_image[-20:])
+
+    f = open(cwd + '\\original.jpg', 'a')
+
+    f.write('\\d9\\d2\\d9')
+
+    f.close()
+
+    # bytes_image.extend(secret)
+
+    bytes_image2 = read_image(cwd + '\\original.jpg')
+
+    # print(bytes_image2[-20:])
+
+    # image = Image.open(io.BytesIO(bytes_image2))
+
+    # image.save("original.jpg")
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
+    main()
