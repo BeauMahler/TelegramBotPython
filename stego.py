e63710dea5619d125cf128710a38b8e713ac5279
bluepython339 <bluepython339@protonmail.com>
2021-11-25 14:51:18 +0100

    stego?

---
+++ stego.py
@@ -0,0 +1,53 @@
+
+
+
+from PIL import Image, ImageFilter
+import numpy as np
+from tqdm import tqdm
+
+class Encoder(object):
+
+
+    def __init__(self, img_name):
+        self.key = []
+        self.img = Image.open(img_name)
+
+
+    def encode_message(self,msg:str):
+        raw_msg = self._pad_msg(msg.encode())
+        for i in tqdm(range(0,len(raw_msg),3), "encoding"):
+            index = self._get_index()
+            self.img.putpixel(index,(raw_msg[i],raw_msg[i+1],raw_msg[i+2]))
+
+    def write_img(self,file_name):
+        self.img.save(file_name, "PNG")
+        print(self.key)
+
+    def _pad_msg(self,msg:bytes):
+        pad_len = 3 - len(msg) % 3
+        return msg+b'\0'*pad_len
+
+    def _get_index(self,):
+        width, height = self.img.size
+        x = np.random.randint(0,width)
+        y = np.random.randint(0,height)
+        if (x,y) in self.key:
+            return self._get_index()
+        else:
+            self.key.append((x,y))
+            return (x,y)
+
+
+class Decoder(object):
+
+    def __init__(self, img_name):
+        self.img = Image.open(img_name)
+
+
+    def decode_msg(self,key):
+        msg = ""
+        for i in tqdm(key, "decoding"):
+            r,g,b = self.img.getpixel(i)
+            msg += chr(r)+chr(g)+chr(b)
+
+        return msg
