322400c6b2fbcc5eafbfd97833dbceaeee6b0570
bluepython339 <bluepython339@protonmail.com>
2021-11-26 11:13:53 +0100

    added png decoding

--- Decoders/MessageDecoder.py
+++ Decoders/MessageDecoder.py
@@ -7,10 +7,17 @@ class MessageDecoder(object):
     def __init__(self, img_name):
         self.__img = Image.open(img_name)
 
-    def decode_msg(self, key):
+    def decode_msg(self,key):
         msg = ""
-        for i in tqdm(key, "decoding"):
-            r, g, b, z = self.__img.getpixel(i)
-            msg += chr(r) + chr(g) + chr(b)
-
-        return msg
+        if len(self.img.getpixel(key[0])) == 4:
+            for i in tqdm(key, "decoding"):
+                print(self.img.getpixel(i))
+                r,g,b, _ = self.img.getpixel(i)
+                msg += chr(r)+chr(g)+chr(b)
+            return msg
+        else:
+            for i in tqdm(key, "decoding"):
+                print(self.img.getpixel(i))
+                r,g,b = self.img.getpixel(i)
+                msg += chr(r)+chr(g)+chr(b)
+            return msg
